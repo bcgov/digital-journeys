b95480a29280f32bc070933e72cccab07ece2ee8
Simen Fivelstad Smaaberg <simen@freshworks.io>
2022-02-04 10:14:57 -0800

    DGJ-11 Added BCGov footers, headers and typography

---
+++ forms-flow-web/src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import "../assets/styles/layouts.scss";
+import "../assets/styles/user-styles.css";
+import BaseRouting from "./BaseRouting";
+import {Helmet} from "react-helmet";
+import {KEYCLOAK_URL} from "../constants/constants";
+import Typography from "./Typography/Typography";
+
+const App = React.memo((props) => {
+    const { store, history } = props;
+    return (
+      <div>
+        <Typography />
+        <Helmet>
+          {KEYCLOAK_URL?<link rel="preconnect" href={KEYCLOAK_URL} />:null}
+        </Helmet>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <BaseRouting store={store}/>
+          </ConnectedRouter>
+        </Provider>
+      </div>
+    );
+});
+
+App.propTypes = {
+  history: PropTypes.any.isRequired,
+  store: PropTypes.any.isRequired,
+};
+
+export default App;
