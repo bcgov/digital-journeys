5c076435ea878d257a47bfc024cfa24d0f0cb902
iman-fw <iman@freshworks.io>
2022-02-07 12:34:36 -0800

    footer style matched with bcgov style

--- forms-flow-web/src/components/Footer/Footer.js
+++ forms-flow-web/src/components/Footer/Footer.js
@@ -1,31 +1,30 @@
 import React from "react";
 import "./footer.scss";
-import {version} from "../../../package.json"
+import { version } from "../../../package.json";
 import Footer from "@button-inc/bcgov-theme/Footer";
-import { Link } from "react-router-dom";
 
 const BCGovFooter = React.memo(() => {
   const footerItems = [
     {
-      href: 'https://www2.gov.bc.ca/gov/content?id=79F93E018712422FBC8E674A67A70535',
-      label: 'Disclaimer',
+      href: "https://www2.gov.bc.ca/gov/content?id=79F93E018712422FBC8E674A67A70535",
+      label: "Disclaimer",
     },
     {
-      href: 'https://www2.gov.bc.ca/gov/content?id=9E890E16955E4FF4BF3B0E07B4722932',
-      label: 'Privacy',
+      href: "https://www2.gov.bc.ca/gov/content?id=9E890E16955E4FF4BF3B0E07B4722932",
+      label: "Privacy",
     },
     {
-      href: 'https://www2.gov.bc.ca/gov/content?id=E08E79740F9C41B9B0C484685CC5E412',
-      label: 'Accessibility',
+      href: "https://www2.gov.bc.ca/gov/content?id=E08E79740F9C41B9B0C484685CC5E412",
+      label: "Accessibility",
     },
     {
-      href: 'https://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA',
-      label: 'Copyright',
+      href: "https://www2.gov.bc.ca/gov/content?id=1AAACC9C65754E4D89A118B875E0FBDA",
+      label: "Copyright",
     },
     {
-      href: 'https://www2.gov.bc.ca/gov/content?id=6A77C17D0CCB48F897F8598CCC019111',
-      label: 'Contact Us',
-    }
+      href: "https://www2.gov.bc.ca/gov/content?id=6A77C17D0CCB48F897F8598CCC019111",
+      label: "Contact Us",
+    },
   ];
 
   return (
@@ -33,7 +32,9 @@ const BCGovFooter = React.memo(() => {
       <ul>
         {footerItems.map((item) => (
           <li>
-            <Link href={item.href} target="_blank">{item.label}</Link>
+            <a href={item.href} target='_blank'>
+              {item.label}
+            </a>
           </li>
         ))}
       </ul>
