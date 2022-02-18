ec72e943bcc8dc0f29dc5be26fbfd71e20dda2a5
kkapoor-fresh <77285719+kkapoor-fresh@users.noreply.github.com>
2022-02-18 16:03:47 +0530

    Delete forms-flow-web directory

--- forms-flow-web/src/components/Typography/Typography.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { TypographyStyle } from 'react-typography';
-import typography from '@button-inc/bcgov-theme/typography';
-import '@bcgov/bc-sans/css/BCSans.css';
-
-export default React.memo(() => <TypographyStyle typography={typography} />);
