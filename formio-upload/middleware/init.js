14980a0d7336f2a152526379e5b11980f83caf2b
Simen Fivelstad Smaaberg <simen@freshworks.io>
2022-03-01 07:57:26 -0800

    DGJ-94 Added formio-upload codebase

---
+++ formio-upload/middleware/init.js
@@ -0,0 +1,18 @@
+const debug = require('debug');
+const { v4: uuid } = require('uuid');
+module.exports = function(namespace) {
+  const log = debug(namespace);
+  return function initialize(req, res, next) {
+    const reqId = uuid();
+    req.cleanup = [];
+    req.debug = (msg) => {
+      log(`${reqId}: ${msg}`);
+    };
+    const ip = req.headers['x-forwarded-for'] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      (req.connection.socket ? req.connection.socket.remoteAddress : null);
+    req.debug(`New Request from ${req.get('Referrer')}: (${ip})`);
+    next();
+  }
+};
