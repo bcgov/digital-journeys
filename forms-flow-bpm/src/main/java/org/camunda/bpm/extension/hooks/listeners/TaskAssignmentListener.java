723b615b087f33be162f27b3fe9d192f6f9ac10d
matbusby-fw <mat@freshworks.io>
2022-03-14 10:31:02 -0600

    Merged pdf functionality into email listener

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/TaskAssignmentListener.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/TaskAssignmentListener.java
@@ -1,9 +1,17 @@
 package org.camunda.bpm.extension.hooks.listeners;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.Margin;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateExceptionHandler;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.extension.hooks.services.FormSubmissionService;
 import org.camunda.bpm.extension.hooks.services.IMessageEvent;
 import org.camunda.bpm.extension.mail.EmptyResponse;
 import org.camunda.bpm.extension.mail.MailConnectorException;
@@ -16,75 +24,178 @@ import org.camunda.bpm.extension.mail.service.MailServiceFactory;
 import org.camunda.connect.impl.AbstractConnector;
 import org.camunda.connect.spi.ConnectorRequestInterceptor;
 import org.camunda.connect.spi.ConnectorResponse;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.inject.Named;
-import javax.mail.Message;
+import javax.mail.*;
 import javax.mail.Message.RecipientType;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.mail.internet.*;
+import javax.ws.rs.core.Application;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.*;
 
 @Named("TaskAssignmentListener")
 public class TaskAssignmentListener extends BaseListener implements TaskListener, IMessageEvent {
 
     protected MailConfiguration configuration;
-    private Expression recipientEmail;
+    private Expression recipientEmails;
     private Expression body;
     private Expression subject;
+    private Expression attachSubmission;
 
     @Value("${formsflow.ai.app.url}")
     private String baseUrl;
+
+    @Autowired
+    private FormSubmissionService formSubmissionService;
 
     public void notify(DelegateTask delegateTask) {
 
         MailService mailService = MailServiceFactory.getService(getConfiguration());
 
-        // Get the string value of the variables passed from the task
-        String recipient = getRecipient(delegateTask.getExecution());
-        String emailBody = getBody(delegateTask.getExecution());
-        String emailSubject = getSubject(delegateTask.getExecution());
-        String taskId = String.valueOf(delegateTask.getId());
+        try {
+            // Get the string value of the variables passed from the task
+            String emailBody = getBody(delegateTask.getExecution());
+            String emailSubject = getSubject(delegateTask.getExecution());
+            String taskId = String.valueOf(delegateTask.getId());
+            Boolean attachPdf = getAttachSubmission(delegateTask.getExecution());
+            InternetAddress[] recipients = getRecipients(delegateTask.getExecution());
 
-        // Create empty request and interceptors for the SendMailInvocation
-        SendMailRequest request = getRequest();
-        List<ConnectorRequestInterceptor> requestInterceptors = getInterceptors();
+            // Create empty request and interceptors for the SendMailInvocation
+            SendMailRequest request = getRequest();
+            List<ConnectorRequestInterceptor> requestInterceptors = getInterceptors();
 
-        if (recipient != null && !recipient.isEmpty()) {
+            if (attachPdf) {
+                // Get the form fields and values data to use in the pdf template
+                Map<String, Object> data = getFormPdfData(delegateTask.getExecution());
 
-            try {
-                Message message = createMessage(recipient, emailBody, emailSubject, taskId, mailService.getSession());
-                SendMailInvocation invocation = new SendMailInvocation(message, request, requestInterceptors, mailService);
+                // Generate the template from '/templates/form.html'
+                Template template = getTemplate();
 
-                invocation.proceed();
+                // Create a temporary 'output.html' file template
+                File f = null;
+                try {
+                    f = File.createTempFile("output", ".html");
+                    template.process(data, new FileWriter(f));
 
-            } catch (Exception e) {
-                throw new MailConnectorException("Failed to send mail: " + e.getMessage(), e);
+                    // Render the file as 'form.pdf'
+                    RenderPage(f);
+                } finally {
+                    if (f != null) {
+                        f.deleteOnExit();
+                    }
+                }
             }
 
+            if (recipients.length > 0) {
+                try {
+                    Message message = createMessage(recipients, emailBody, emailSubject, taskId, attachPdf, mailService.getSession());
+                    SendMailInvocation invocation = new SendMailInvocation(message, request, requestInterceptors, mailService);
+
+                    invocation.proceed();
+
+                } catch (Exception e) {
+                    throw new MailConnectorException("Failed to send mail: " + e.getMessage(), e);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public Message createMessage(String recipient, String body, String subject, String taskId, Session session) throws Exception {
+    private Map<String, Object> getFormPdfData(DelegateExecution execution) throws IOException {
+        // Get Form Values
+        String formUrlString = String.valueOf(execution.getVariables().get("formUrl"));
+        JSONObject formValues = formSubmissionService.retrieveFormJson(formUrlString).getJSONObject("data");
+
+        // Get the URL for the form fields
+        String formUrl = formUrlString.split("/submission/")[0];
+        JSONObject formFields = formSubmissionService.retrieveFormJson(formUrl);
+
+        // Extract the emails for the manager and submitter
+        Map<String, Object> valuesDataMap = formSubmissionService.retrieveFormValues(formUrlString);
+//        managerEmail = String.valueOf(valuesDataMap.get("managerEmail"));
+//        submitterEmail = String.valueOf(valuesDataMap.get("email"));
+
+        // Create the data to be passed to the template
+        Map<String, Object> data = new HashMap<>();
+        data.put("formFields", formFields);
+        data.put("formValues", formValues);
+        return data;
+    }
+
+    public Message createMessage(InternetAddress[] recipients, String body, String subject, String taskId, Boolean attachPdf, Session session) throws Exception {
         MailConfiguration configuration = getConfiguration();
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress(configuration.getSender(), configuration.getSenderAlias()));
-        message.setRecipients(RecipientType.TO, InternetAddress.parse(recipient));
+        Address[] addresses;
+        message.setRecipients(RecipientType.TO, recipients);
 
-        message.setSentDate(new Date());
-        message.setSubject(subject);
+        // Create a multipart message
+        Multipart multipart = new MimeMultipart();
 
+        // Create the body text of the email
         String bodyUrl = body.replace("$BASE_URL", baseUrl);
         String finalBody = bodyUrl.replace("$TASK_ID", taskId);
-
+        BodyPart messageBodyPart = new MimeBodyPart();
         if (!body.isEmpty()) {
-            message.setText(finalBody);
+            messageBodyPart.setText(finalBody);
         } else {
             message.setText("");
         }
+        multipart.addBodyPart(messageBodyPart);
+
+        // Create the pdf attachment if attach is true
+        if (attachPdf) {
+            messageBodyPart = new MimeBodyPart();
+            String filename = "form.pdf";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);
+        }
+
+        message.setSentDate(new Date());
+        message.setContent(multipart);
+        message.setSubject(subject);
 
         return message;
+    }
+
+    private Template getTemplate() throws IOException {
+        // Instantiate the FreekMarker config
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setClassForTemplateLoading(Application.class, "/templates");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
+        Template template = cfg.getTemplate("/form.html");
+        return template;
+    }
+
+    private void RenderPage(File file) {
+        try (Playwright playwright = Playwright.create()) {
+
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+
+            page.navigate(file.toPath().toUri().toURL().toString());
+            Thread.sleep(500);
+
+            page.pdf(new Page.PdfOptions().setPath(Paths.get("form.pdf")).setMargin(new Margin().setRight("40px").setLeft("40px").setTop("40px").setBottom("40px")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -98,11 +209,6 @@ public class TaskAssignmentListener extends BaseListener implements TaskListener
         }
         return configuration;
     }
-
-    public void setConfiguration(MailConfiguration configuration) {
-        this.configuration = configuration;
-    }
-
 
     /**
      * Generate empty interceptors
@@ -252,12 +358,22 @@ public class TaskAssignmentListener extends BaseListener implements TaskListener
         return String.valueOf(this.subject.getValue(delegateExecution));
     }
 
-    private String getRecipient(DelegateExecution delegateExecution) {
-        return String.valueOf(this.recipientEmail.getValue(delegateExecution));
+    private InternetAddress[] getRecipients(DelegateExecution delegateExecution) throws AddressException {
+        String[] recipientArray = String.valueOf(this.recipientEmails.getValue(delegateExecution)).split(",");
+        InternetAddress[] recipients = new InternetAddress[recipientArray.length];
+        for (int i = 0; i < recipientArray.length; i++) {
+            recipients[i] = InternetAddress.parse(recipientArray[i])[0];
+        }
+        return recipients;
     }
 
     private String getBody(DelegateExecution delegateExecution) {
         return String.valueOf(this.body.getValue(delegateExecution));
     }
+
+    private Boolean getAttachSubmission(DelegateExecution delegateExecution) {
+        return Boolean.parseBoolean((String) this.attachSubmission.getValue(delegateExecution));
+    }
+
 
 }
