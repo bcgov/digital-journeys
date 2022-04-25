8c90ed69e4ed0ecd202b17869040a7724a2d9831
jizhaogit <ji.zhao@telus.com>
2022-04-25 08:49:43 -0400

    Create ExtractManagerGUID.java

---
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/listeners/ExtractManagerGUID.java
@@ -0,0 +1,55 @@
+package org.camunda.bpm.extension.hooks.listeners;
+
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.extension.hooks.services.FormSubmissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Named;
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * This class transforms all the form document data into CAM variables
+ *
+ * @author sumathi.thirumani@aot-technologies.com
+ */
+@Named("ExtractManagerGUID")
+public class ExtractManagerGUID  extends BaseListener implements TaskListener, ExecutionListener {
+
+    @Autowired
+    private FormSubmissionService formSubmissionService;
+
+    @Override
+    public void notify(DelegateExecution execution) {
+        try {
+            syncFormVariables(execution);
+        } catch (IOException e) {
+            handleException(execution, ExceptionSource.EXECUTION, e);
+        }
+    }
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        try {
+            syncFormVariables(delegateTask.getExecution());
+        } catch (IOException e) {
+            handleException(delegateTask.getExecution(), ExceptionSource.TASK, e);
+        }
+    }
+
+    private void syncFormVariables(DelegateExecution execution) throws IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+        String guid = oidcUser.getClaimAsString("bcgovguid");
+        String idir = oidcUser.getClaimAsString("idir");
+
+        execution.setVariable("managerGuid", guid);
+        execution.setVariable("managerIdir", idir);    
+
+    }
+}
