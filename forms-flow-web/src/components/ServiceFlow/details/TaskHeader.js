243ca7d2ecba6768297a91c5ce7c6d001b5b7b62
David Sabadash <DRsabadash@gmail.com>
2022-07-08 00:20:57 -0700

    imported Service Flow component into local repository, imported user role selector into task header component, added role-based conditional rendering to the date selection elements and the reassign element

---
+++ forms-flow-web/src/components/ServiceFlow/details/TaskHeader.js
@@ -0,0 +1,249 @@
+import React, {useEffect, useState} from "react";
+import { Row, Col } from "react-bootstrap";
+import {
+  getISODateTime,
+  getProcessDataFromList,
+  getFormattedDateAndTime
+} from "../../../apiManager/services/formatterService";
+import {useDispatch, useSelector} from "react-redux";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
+import "./../ServiceFlow.scss";
+import AddGroupModal from "./AddGroupModal";
+import {
+  claimBPMTask,
+  fetchServiceTaskList,
+  getBPMTaskDetail,
+  unClaimBPMTask, updateAssigneeBPMTask,
+  updateBPMTask
+} from "../../../apiManager/services/bpmTaskServices";
+import {setBPMTaskDetailUpdating} from "../../../actions/bpmTaskActions";
+import {getUserRoleName, getUserRolePermission, getUserInsightsPermission} from "../../../helper/user";
+import {CLIENT, STAFF_REVIEWER, APPLICATION_NAME, STAFF_DESIGNER} from "../../../constants/constants";
+
+//import UserSelection from "./UserSelection";
+import UserSelectionDebounce from "./UserSelectionDebounce";
+import SocketIOService from "../../../services/SocketIOService";
+
+const TaskHeader = React.memo(() => {
+  const task = useSelector(state => state.bpmTasks.taskDetail);
+  const taskId = useSelector((state) => state.bpmTasks.taskId);
+  const processList = useSelector((state) => state.bpmTasks.processList);
+  const username = useSelector((state) => state.user?.userDetail?.preferred_username || '');
+  const taskGroups = useSelector(state=>state.bpmTasks.taskGroups);
+  const selectedFilter=useSelector(state=>state.bpmTasks.selectedFilter);
+  const reqData = useSelector(state => state.bpmTasks.listReqParams);
+  const firstResult = useSelector(state=> state.bpmTasks.firstResult);
+  const userRoles = useSelector((state) => state.user.roles);
+  const [followUpDate, setFollowUpDate] = useState(null);
+  const [dueDate, setDueDate] = useState(null);
+  const [showModal, setModal] = useState(false);
+  const [isEditAssignee, setIsEditAssignee]=useState(false);
+  const dispatch= useDispatch();
+
+  useEffect(()=>{
+    const followUp= task?.followUp ? new Date(task?.followUp):null;
+    setFollowUpDate(followUp);
+  },[task?.followUp])
+
+  useEffect(()=>{
+    const due= task?.due ? new Date(task?.due): null;
+    setDueDate(due);
+  },[task?.due]);
+
+  const onClaim = () => {
+    dispatch(setBPMTaskDetailUpdating(true));
+    dispatch(claimBPMTask(taskId,username,(err,response)=>{
+      if(!err){
+        if(!SocketIOService.isConnected()){
+          if(selectedFilter){
+            dispatch(getBPMTaskDetail(taskId));
+            dispatch(fetchServiceTaskList(selectedFilter.id, firstResult, reqData));
+          }else{
+            dispatch(setBPMTaskDetailUpdating(false));
+          }
+        }
+      }else{
+        dispatch(setBPMTaskDetailUpdating(false));
+      }
+    }));
+  }
+  const onChangeClaim = (userId) => {
+    setIsEditAssignee(false);
+   if(userId && userId!==task.assignee){
+     dispatch(setBPMTaskDetailUpdating(true));
+     dispatch(updateAssigneeBPMTask(taskId,userId,(err,response)=>{
+       if(!err){
+         if(!SocketIOService.isConnected()){
+         if(selectedFilter){
+           dispatch(getBPMTaskDetail(taskId));
+           dispatch(fetchServiceTaskList(selectedFilter.id, firstResult, reqData));
+         }
+         }
+       }else{
+         dispatch(setBPMTaskDetailUpdating(false));
+       }
+     }));
+   }
+  }
+
+  const onUnClaimTask = () =>{
+    dispatch(setBPMTaskDetailUpdating(true));
+    dispatch(unClaimBPMTask(taskId,(err,response)=>{
+      if(!err){
+        if(!SocketIOService.isConnected()){
+        if(selectedFilter){
+          dispatch(getBPMTaskDetail(taskId));
+          dispatch(fetchServiceTaskList(selectedFilter.id, firstResult, reqData));
+        }
+        }
+      }else{
+        dispatch(setBPMTaskDetailUpdating(false));
+      }
+    }));
+  }
+
+  const onFollowUpDateUpdate = (followUpDate)=>{
+    setFollowUpDate(followUpDate);
+    dispatch(setBPMTaskDetailUpdating(true));
+    const updatedTask = {...task, ...{followUp:followUpDate?getISODateTime(followUpDate):null}};
+    dispatch(updateBPMTask(taskId,updatedTask,(err,response)=>{
+      if(!err){
+        if(!SocketIOService.isConnected()) {
+          dispatch(getBPMTaskDetail(taskId));
+          dispatch(fetchServiceTaskList(selectedFilter.id, firstResult, reqData));
+        }
+      }else{
+        dispatch(setBPMTaskDetailUpdating(false));
+      }
+    }))
+  };
+
+  const onDueDateUpdate = (dueDate)=>{
+    setDueDate(dueDate);
+    dispatch(setBPMTaskDetailUpdating(true));
+    const updatedTask = {...task, ...{due:dueDate?getISODateTime(dueDate):null}};
+    dispatch(updateBPMTask(taskId,updatedTask,(err,response)=>{
+      if(!err){
+        if(!SocketIOService.isConnected()) {
+          dispatch(getBPMTaskDetail(taskId));
+          dispatch(fetchServiceTaskList(selectedFilter.id, firstResult, reqData));
+        }
+      }else{
+        dispatch(setBPMTaskDetailUpdating(false));
+      }
+    }))
+  };
+
+  const FollowUpDateInput= React.forwardRef(({ value, onClick }, ref) =>{
+   return    <div onClick={onClick} ref={ref}>
+      <i className="fa fa-calendar mr-1"/>{" "}
+      {followUpDate
+        ? <span className="mr-4">{moment(followUpDate).fromNow()}</span>
+        : "Set follow-up Date"}
+    </div>
+  });
+
+
+
+  const DueDateInput=React.forwardRef(({ value, onClick }, ref) =>{
+    return    <div onClick={onClick} ref={ref}>
+     <i className="fa fa-bell mr-1"/>{" "}
+      {dueDate ? <span className="mr-4">{moment(dueDate).fromNow()}</span> : "Set Due date"}
+    </div>
+  });
+
+  const getGroups = (groups)=>{
+    return groups?.map(group=>group.groupId).join(", ");
+  }
+
+  return (
+    <>
+    <AddGroupModal modalOpen={showModal} onClose={()=>setModal(false)} groups={taskGroups}/>
+      <Row className="ml-0 task-header">{task?.name}</Row>
+      <Row className="ml-0 task-name" >
+      <span className="application-id" dat-title={"Process Name"}> {getProcessDataFromList(processList, task?.processDefinitionId, "name")}</span>
+      </Row>
+      <Row className="ml-0" >
+      <span data-title="Application Id" className="application-id"> Application ID# {task?.applicationId}</span>
+      </Row>
+      <Row className="actionable mb-4">
+        { !getUserRolePermission(userRoles, STAFF_REVIEWER) 
+            &&
+          <Col sm={followUpDate?2:"auto"} data-title={followUpDate?getFormattedDateAndTime(followUpDate):'Set FollowUp Date'} className='date-container'>
+            <DatePicker
+              selected={followUpDate}
+              onChange={onFollowUpDateUpdate}
+              showTimeSelect
+              isClearable
+              popperPlacement="bottom-start"
+              popperModifiers={{
+                offset: {
+                  enabled: true,
+                  offset: "5px, 10px"
+                },
+                preventOverflow: {
+                  enabled: true,
+                  escapeWithReference: false,
+                  boundariesElement: "viewport"
+                }
+              }}
+              customInput={<FollowUpDateInput/>}
+            />
+          </Col>
+        }
+        { !getUserRolePermission(userRoles, STAFF_REVIEWER) 
+            &&
+          <Col sm={dueDate?2:"auto"} data-title={dueDate?getFormattedDateAndTime(dueDate):'Set Due Date'} className='date-container'>
+            <DatePicker
+              selected={dueDate}
+              onChange={onDueDateUpdate}
+              showTimeSelect
+              isClearable
+              shouldCloseOnSelect
+              popperPlacement="bottom-start"
+              popperModifiers={{
+                offset: {
+                  enabled: true,
+                  offset: "5px, 10px"
+                },
+                preventOverflow: {
+                  enabled: true,
+                  escapeWithReference: false,
+                  boundariesElement: "viewport"
+                }
+              }}
+              customInput={<DueDateInput/>}
+            />
+          </Col>
+        }
+        <Col className="center-position" sm={4} onClick={()=>setModal(true)} dat-title={"groups"}>
+          <i className="fa fa-group mr-1"/>
+          { taskGroups.length === 0? <span>Add groups</span>:<span className="group-align">{getGroups(taskGroups)}</span>}
+        </Col>
+        { !getUserRolePermission(userRoles, STAFF_REVIEWER) 
+            &&
+          <Col className="right-side">
+            {isEditAssignee?(task?.assignee? <span>
+                <UserSelectionDebounce onClose={()=>setIsEditAssignee(false)}
+                              currentUser={task.assignee}
+                              onChangeClaim={onChangeClaim}/></span>:
+              <span data-testid="clam-btn" onClick={onClaim}> Claim</span>):
+              (<>
+            <i className="fa fa-user mr-1" />
+            {task?.assignee ? (<span>
+                <span className="change-tooltip" onClick={()=>setIsEditAssignee(true)} dat-title="Click to Change Assignee">{task.assignee}</span>
+                <i className="fa fa-times ml-1" onClick={onUnClaimTask} dat-title="Reset Assignee"/></span>) :
+                <span data-testid="clam-btn" onClick={onClaim}> Claim</span>
+              }
+              </>)
+            }
+          </Col>
+        }
+      </Row>
+    </>
+  );
+});
+
+export default TaskHeader;
---
+++ forms-flow-web/src/components/ServiceFlow/index.js
@@ -0,0 +1,152 @@
+import React, {useCallback, useEffect, useRef} from 'react'
+import ServiceFlowTaskList from "./list/ServiceTaskList";
+import ServiceFlowTaskDetails from "./details/ServiceTaskDetails";
+import {Col, Container, Row} from "react-bootstrap";
+import "./ServiceFlow.scss";
+import {
+  fetchFilterList,
+  fetchProcessDefinitionList,
+  fetchServiceTaskList,
+  getBPMGroups, getBPMTaskDetail
+} from "../../apiManager/services/bpmTaskServices";
+import {useDispatch, useSelector} from "react-redux";
+import {ALL_TASKS} from "./constants/taskConstants";
+import {
+  reloadTaskFormSubmission,
+  setBPMFilterLoader,
+  setBPMTaskDetailLoader,
+  setFilterListParams,
+  setSelectedBPMFilter, setSelectedTaskID
+} from "../../actions/bpmTaskActions";
+import TaskSortSelectedList from "./list/sort/TaskSortSelectedList";
+import SocketIOService from "../../services/SocketIOService";
+import isEqual from 'lodash/isEqual';
+import cloneDeep from 'lodash/cloneDeep';
+import {Route, Redirect} from "react-router-dom";
+import {push} from "connected-react-router";
+
+export default React.memo(() => {
+  const dispatch= useDispatch();
+  const filterList = useSelector(state=> state.bpmTasks.filterList);
+  const isFilterLoading = useSelector(state=> state.bpmTasks.isFilterLoading);
+  const selectedFilter=useSelector(state=>state.bpmTasks.selectedFilter);
+  const selectedFilterId=useSelector(state=>state.bpmTasks.selectedFilter?.id||null);
+  const bpmTaskId = useSelector(state => state.bpmTasks.taskId);
+  const reqData = useSelector((state) => state.bpmTasks.listReqParams);
+  const sortParams = useSelector((state) => state.bpmTasks.filterListSortParams);
+  const searchParams = useSelector((state) => state.bpmTasks.filterListSearchParams);
+  const listReqParams = useSelector((state) => state.bpmTasks.listReqParams);
+  const currentUser = useSelector((state) => state.user?.userDetail?.preferred_username || '');
+  const firstResult = useSelector(state=> state.bpmTasks.firstResult);
+  const taskList = useSelector((state) => state.bpmTasks.tasksList);
+  const selectedFilterIdRef=useRef(selectedFilterId);
+  const bpmTaskIdRef=useRef(bpmTaskId);
+  const reqDataRef=useRef(reqData);
+  const firstResultsRef=useRef(firstResult);
+  const taskListRef=useRef(taskList);
+
+  useEffect(()=>{
+    selectedFilterIdRef.current=selectedFilterId;
+    bpmTaskIdRef.current=bpmTaskId;
+    reqDataRef.current=reqData;
+    firstResultsRef.current=firstResult;
+    taskListRef.current=taskList;
+  });
+
+  useEffect(()=>{
+    const reqParamData={...{sorting:[...sortParams.sorting]},...searchParams};
+    if(!isEqual(reqParamData,listReqParams)){
+      dispatch(setFilterListParams(cloneDeep(reqParamData)))
+    }
+  },[searchParams,sortParams,dispatch,listReqParams])
+
+  useEffect(()=>{
+    dispatch(setBPMFilterLoader(true));
+    dispatch(fetchFilterList());
+    dispatch(fetchProcessDefinitionList());
+    // dispatch(fetchUserList());
+  },[dispatch]);
+
+  useEffect(()=>{
+    if(!isFilterLoading && filterList.length && !selectedFilter){
+      let filterSelected;
+      if(filterList.length>1){
+        filterSelected = filterList.find(filter=> filter.name===ALL_TASKS);
+        if(!filterSelected){
+          filterSelected=filterList[0];
+        }
+      }else {
+        filterSelected = filterList[0];
+      }
+      dispatch(setSelectedBPMFilter(filterSelected));
+    }
+  },[filterList,isFilterLoading,selectedFilter,dispatch]);
+
+  const checkIfTaskIDExistsInList = (list,id)=>{
+    return list.some(task=>task.id===id);
+  }
+  const SocketIOCallback = useCallback((refreshedTaskId, forceReload, isUpdateEvent) => {
+      if(forceReload){
+        dispatch(fetchServiceTaskList(selectedFilterIdRef.current, firstResultsRef.current, reqDataRef.current,refreshedTaskId)); //Refreshes the Tasks
+        if(bpmTaskIdRef.current && refreshedTaskId===bpmTaskIdRef.current){
+          dispatch(setBPMTaskDetailLoader(true));
+          dispatch(setSelectedTaskID(null)); // unSelect the Task Selected
+          dispatch(push(`/task/`));
+        }
+      } else{
+        if(selectedFilterIdRef.current){
+          if(isUpdateEvent){
+            /* Check if the taskId exists in the loaded Task List */
+            if(checkIfTaskIDExistsInList(taskListRef.current,refreshedTaskId)===true){
+              dispatch(fetchServiceTaskList(selectedFilterIdRef.current, firstResultsRef.current, reqDataRef.current)); //Refreshes the Task
+            }
+          }else{
+            dispatch(fetchServiceTaskList(selectedFilterIdRef.current, firstResultsRef.current, reqDataRef.current)); //Refreshes the Task
+          }
+        }
+        if(bpmTaskIdRef.current && refreshedTaskId===bpmTaskIdRef.current) { //Refreshes task if its selected
+          dispatch(getBPMTaskDetail(bpmTaskIdRef.current,(err,resTask)=>{
+            // Should dispatch When task claimed user  is not the logged in User
+            if(resTask?.assignee!==currentUser){
+              dispatch(reloadTaskFormSubmission(true));
+            }
+          }));
+          dispatch(getBPMGroups(bpmTaskIdRef.current));
+        }
+      }
+    }
+  ,[dispatch,currentUser]);
+
+  useEffect(()=>{
+    if(!SocketIOService.isConnected()){
+        SocketIOService.connect((refreshedTaskId, forceReload, isUpdateEvent) => SocketIOCallback(refreshedTaskId, forceReload, isUpdateEvent));
+    }else{
+        SocketIOService.disconnect();
+        SocketIOService.connect((refreshedTaskId, forceReload, isUpdateEvent) => SocketIOCallback(refreshedTaskId, forceReload, isUpdateEvent));
+    }
+    return ()=>{
+      if(SocketIOService.isConnected())
+        SocketIOService.disconnect();
+    }
+  },[SocketIOCallback,dispatch]);
+
+
+  return (
+    <Container fluid id="main" className="pt-0">
+      <Row>
+        <Col lg={3} xs={12} sm={12} md={4} xl={3}>
+          <section>
+            <header className="task-section-top">
+              <TaskSortSelectedList/>
+            </header>
+              <ServiceFlowTaskList/>
+          </section>
+        </Col>
+        <Col className="pl-0" lg={9} xs={12} sm={12} md={8} xl={9}>
+          <Route path={"/task/:taskId?"}><ServiceFlowTaskDetails/></Route>
+          <Route path={"/task/:taskId/:notAvailable"}> <Redirect exact to='/404'/></Route>
+        </Col>
+      </Row>
+    </Container>
+  )
+});
