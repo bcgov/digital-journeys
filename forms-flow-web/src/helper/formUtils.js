bcf33445058e0d45b07b1857be94be193e633047
iman-fw <iman@freshworks.io>
2022-11-09 20:03:50 +0000

    printPdf button was enabled in submission.view page where form is readonly

--- forms-flow-web/src/helper/formUtils.js
+++ forms-flow-web/src/helper/formUtils.js
@@ -27,4 +27,21 @@ const scrollToErrorOnValidation = (formio, scrollToErrorInterval) => {
   }
 };
 
-export { convertFormLinksToOpenInNewTabs, scrollToErrorOnValidation };
+const enableFormButton = (formio, enableButtonInterval, buttonKey, onClick) => {
+  if (formio) {
+    clearInterval(enableButtonInterval);
+    const button = document.getElementsByName(`data[${buttonKey}]`)[0];
+    if (button) {
+      button.removeAttribute("disabled");
+      if (onClick) {
+        button.onclick = onClick;
+      }
+    }
+  }
+};
+
+export {
+  convertFormLinksToOpenInNewTabs,
+  scrollToErrorOnValidation,
+  enableFormButton,
+};
