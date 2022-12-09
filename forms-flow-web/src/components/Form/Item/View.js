30e3b28dd5cebea7e47428da355e2215a465a20a
iman-fw <iman@freshworks.io>
2022-12-09 16:05:38 +0000

    loading was uncommented

--- forms-flow-web/src/components/Form/Item/View.js
+++ forms-flow-web/src/components/Form/Item/View.js
@@ -513,7 +513,7 @@ const View = React.memo((props) => {
       </div>
       <Errors errors={errors} />
       <LoadingOverlay
-        // active={isFormSubmissionLoading || employeeData.loading}
+        active={isFormSubmissionLoading || employeeData.loading}
         spinner
         // text={<Translation>{(t) => t("Loading...")}</Translation>}
         text={
