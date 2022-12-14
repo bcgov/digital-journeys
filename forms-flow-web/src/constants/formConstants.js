2ad3d75d80bba42b43e2068715ab067f52b2bcf4
bhumin-fw <99269796+bhumin-fw@users.noreply.github.com>
2022-12-14 21:28:06 +0530

    DGJ-748 new name for article 1 10 (#797)
    * DGJ-748 Add the new name of article 1.10

--- forms-flow-web/src/constants/formConstants.js
+++ forms-flow-web/src/constants/formConstants.js
@@ -1,9 +1,8 @@
-
-
 export const FORM_NAMES = {
     slreview: 'SL review',
     srleadershipreview: 'Senior Leadership Review Form',
     teleworkagreement: 'Telework agreement',
     complaintintakeform: 'Complaint Intake Form',
-    complaintform: 'Bullying and Harassment Complaint Form'
+    complaintform: 'Bullying and Harassment Complaint Form',
+    complaintform110: 'Bully / Misuse of Authority Complaint'
   };
