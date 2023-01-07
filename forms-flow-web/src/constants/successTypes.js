b63f175019aac84acd1681426a71be4cc1424aab
bhumin-fw <99269796+bhumin-fw@users.noreply.github.com>
2023-01-07 10:33:04 +0530

    DGJ-794 Change 1.10 form success page content based on PSA or LDB (#840)
    * DGJ-794 Change 1.10 form success page content based on PSA or LDB

--- forms-flow-web/src/constants/successTypes.js
+++ forms-flow-web/src/constants/successTypes.js
@@ -5,14 +5,22 @@ export const SL_REVIEW_SUBMISSION = "SL_REVIEW_SUBMISSION";
 export const SL_REVIEW_RESUBMISSION = "SL_REVIEW_RESUBMISSION";
 export const SL_REVIEW_FINAL_SUBMISSION = "SL_REVIEW_FINAL_SUBMISSION";
 export const COMPLAINT_INTAKE_FORM_1_10 = "1.10_COMPLAINT_INTAKE_FORM";
+export const COMPLAINT_INTAKE_FORM_1_10_LDB = "1.10_COMPLAINT_INTAKE_FORM_LDB";
 
-let submitSuccessPage = {
+const submitSuccessPage = {
   seniorleadershipreview: SL_REVIEW_SUBMISSION,
   teleworkagreement: TELEWORK_SUBMISSION,
   "bullying-and-harassment-complaint-article-1-10": COMPLAINT_INTAKE_FORM_1_10,
+  "bullying-and-harassment-complaint-article-1-10-ldb": COMPLAINT_INTAKE_FORM_1_10_LDB,
 };
 
-export const redirectToFormSuccessPage = (dispatch, push, formKey) => {
+export const redirectToFormSuccessPage = (dispatch, push, formKey, submission) => {
+    if (formKey === "bullying-and-harassment-complaint-article-1-10") {
+      if (submission?.data?.pleaseSelectTheUnionYouBelongTo === "bcGeneralEmployeesUnionBcgeu"
+      && submission?.data?.areYouAnEmployeeOfTheLiquorDistributionBoardLdb === "yes") {
+            formKey = `${formKey}-ldb`;
+      }
+    }
     return redirectToSuccessPage(dispatch, push, submitSuccessPage[formKey]);
 };
 
