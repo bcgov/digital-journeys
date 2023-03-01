8c25e0fde9b24823f899da520debfa4a2692fe13
iman-jamali-fw <77303486+iman-jamali-fw@users.noreply.github.com>
2023-03-01 09:47:18 -0800

    fixed the issue with the previous eventListners of tooltip info button sometimes triggered (#944)

--- forms-flow-web/src/formComponents/TextField/index.js
+++ forms-flow-web/src/formComponents/TextField/index.js
@@ -6,20 +6,27 @@ const TextField = Components.components.textfield;
  * Overrides the default TextField component
  *
  * TextField will support tooltip that is displayed as a popup
- * 
+ *
  */
 export default class DGJTextFieldComponent extends TextField {
   attach(element) {
     const webForm = this;
     const tooltipInfoButton = element?.children[0]?.children[0];
     const tooltipHtmlText = webForm.component.tooltip;
-    // By contract, form designer should add the following 
+    // By contract, form designer should add the following
     // popup html comment in the tooltip text to force it to be opened as a popup
     const isTooltipPopup = tooltipHtmlText.includes("<!-- popup -->");
     if (isTooltipPopup && tooltipInfoButton) {
       // Removing tooltip text before popup to disable the default tooltip display
       tooltipInfoButton.setAttribute("data-tooltip", "");
-      tooltipInfoButton.addEventListener("click", function () {
+      // Replacing the tooltipButton with a clone to make sure all the 
+      // previous eventListeners are removed except the popup
+      let newTooltipInfoButton = tooltipInfoButton.cloneNode(true);
+      tooltipInfoButton.parentNode.replaceChild(
+        newTooltipInfoButton,
+        tooltipInfoButton
+      );
+      newTooltipInfoButton.addEventListener("click", function () {
         webForm.emit("customEvent", {
           type: "popup",
           body: tooltipHtmlText,
