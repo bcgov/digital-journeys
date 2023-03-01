f80f0560ae3fd248166f65f4155d6f140eac9fd3
iman-fw <iman@freshworks.io>
2023-03-01 17:38:31 +0000

    fixed the issue with the previous eventListners of tooltip info button sometimes triggered

--- forms-flow-web/src/formComponents/TextArea/index.js
+++ forms-flow-web/src/formComponents/TextArea/index.js
@@ -13,13 +13,20 @@ export default class DGJTextAreaComponent extends TextArea {
     const webForm = this;
     const tooltipInfoButton = element?.children[0]?.children[0];
     const tooltipHtmlText = webForm.component.tooltip;
-    // By contract, form designer should add the following 
+    // By contract, form designer should add the following
     // popup html comment in the tooltip text to force it to be opened as a popup
     const isTooltipPopup = tooltipHtmlText.includes("<!-- popup -->");
     if (isTooltipPopup && tooltipInfoButton) {
       // Removing tooltip text before popup to disable the default tooltip display
       tooltipInfoButton.setAttribute("data-tooltip", "");
-      tooltipInfoButton.addEventListener("click", function () {
+      // Replacing the tooltipButton with a clone to make sure all the previous 
+      // eventListeners are removed except the popup
+      let newTooltipInfoButton = tooltipInfoButton.cloneNode(true);
+      tooltipInfoButton.parentNode.replaceChild(
+        newTooltipInfoButton,
+        tooltipInfoButton
+      );
+      newTooltipInfoButton.addEventListener("click", function () {
         webForm.emit("customEvent", {
           type: "popup",
           body: tooltipHtmlText,
