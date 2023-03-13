5fe2b4332882b0a71406ec5e76f66319ee4ede53
iman-fw <iman@freshworks.io>
2023-03-13 20:09:48 +0000

    ldb user autopopulate was added

--- forms-flow-api/src/formsflow_api/models/employee_data_bceid.py
+++ forms-flow-api/src/formsflow_api/models/employee_data_bceid.py
@@ -1,4 +1,4 @@
-class EmployeeDataBceid():
+class EmployeeDataFromKeycloak():
   def __init__(self, data):
    self.displayName = " ".join(filter(None, (data["first_name"], data["last_name"])))
    self.firstName = data["first_name"]
