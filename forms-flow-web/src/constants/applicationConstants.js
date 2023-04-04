8e807bce4841d45c66f715c76a0638d99ba84da3
iman-jamali-fw <77303486+iman-jamali-fw@users.noreply.github.com>
2023-04-04 14:27:11 -0700

    feature/DGJ_906-edit-submitted-mat_pat-form (#1026)
    * hide the wizzard form submit btn and allow for two more client edit status used for mat-pat form
    
    * mat-pat workflow was added
    
    * addressed comments

--- forms-flow-web/src/constants/applicationConstants.js
+++ forms-flow-web/src/constants/applicationConstants.js
@@ -2,13 +2,19 @@
 export const RESUBMITTED_STATUS_EVENT = "application_resubmitted";
 export const ACKNOWLEDGED_EVENT = "application_acknowledged";
 
-//export const RETURNED_STATUS = "Returned";
 export const RESUBMIT_STATUS = "Resubmit";
 export const AWAITING_ACKNOWLEDGEMENT = "Awaiting Acknowledgement";
 export const SUBMITTED_STATUS = "Submitted";
-//export const NEW_STATUS = "New";
+export const RESUBMITTED_STATUS = "Resubmitted";
+export const VALIDATED_STATUS = "Validated";
 
-export const CLIENT_EDIT_STATUS = [AWAITING_ACKNOWLEDGEMENT, RESUBMIT_STATUS, SUBMITTED_STATUS];
+export const CLIENT_EDIT_STATUS = [
+  AWAITING_ACKNOWLEDGEMENT,
+  RESUBMIT_STATUS,
+  SUBMITTED_STATUS,
+  RESUBMITTED_STATUS,
+  VALIDATED_STATUS,
+];
 
 export const UPDATE_EVENT_STATUS = [RESUBMIT_STATUS, AWAITING_ACKNOWLEDGEMENT];
 
