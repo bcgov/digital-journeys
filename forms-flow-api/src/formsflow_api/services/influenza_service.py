aeb7b0e2545e7d468c127b1556c3b691210104a2
bhumin-fw <99269796+bhumin-fw@users.noreply.github.com>
2023-04-17 20:18:12 +0530

    DGJ-1024 workflow supporting API endpoints and data transform (#1037)

--- forms-flow-api/src/formsflow_api/services/influenza_service.py
+++ forms-flow-api/src/formsflow_api/services/influenza_service.py
@@ -10,7 +10,7 @@ class InfluenzaService:
     def get_ministry_names():
         """fetch ministry_names"""
         try:
-            ministry_api_url = "{}/ODS_Datamart_influenza_ministry".format(current_app.config.get("ODS_URL"))
+            ministry_api_url = "{}/ods_datamart_influenza_ministries".format(current_app.config.get("ODS_URL"))
             response_from_ods = requests.get(ministry_api_url,
                         headers={"Authorization": current_app.config.get("ODS_AUTH_TOKEN")})
         except Exception as e:
@@ -26,37 +26,11 @@ class InfluenzaService:
         )
     
     @staticmethod
-    def get_cities():
-        """fetch cities"""
-        try:
-            cities_api_url = "{}/ODS_Datamart_influenza_city".format(current_app.config.get("ODS_URL"))
-            response_from_ods = requests.get(cities_api_url,
-                        headers={"Authorization": current_app.config.get("ODS_AUTH_TOKEN")})
-        except Exception as e:
-            raise BusinessException(
-                {"message": "Failed to look up cities in ODS"}, HTTPStatus.INTERNAL_SERVER_ERROR
-            )
-        
-        cities_res = response_from_ods.json()
-        if cities_res and "value" in cities_res and len(cities_res["value"]) > 0:
-            return cities_res["value"]
-        raise BusinessException(
-          {"message": "No city data found"}, HTTPStatus.NOT_FOUND
-        )
-    
-    @staticmethod
     def get_worksites(args):
         """fetch influenza worksites"""
         try:
-            worksites_api_url = "{}/ODS_Datamart_influenza_worksite".format(current_app.config.get("ODS_URL"))
-            mid = args.get("mid", None) # Ministry ID
-            query = ""
-            if mid:
-                query = f"mID eq {mid}"
-            if query != "":
-                query = f"$filter={query}"
-            url="{}?{}".format(worksites_api_url, query)
-            response_from_ods = requests.get(url,
+            worksites_api_url = "{}/ods_datamart_influenza_worksites".format(current_app.config.get("ODS_URL"))
+            response_from_ods = requests.get(worksites_api_url,
                         headers={"Authorization": current_app.config.get("ODS_AUTH_TOKEN")})
         except Exception as e:
             raise BusinessException(
@@ -66,6 +40,25 @@ class InfluenzaService:
         worksites_res = response_from_ods.json()
         if worksites_res and "value" in worksites_res and len(worksites_res["value"]) > 0:
             return worksites_res["value"]
+        raise BusinessException(
+          {"message": "No worksite data found"}, HTTPStatus.NOT_FOUND
+        )
+    
+    @staticmethod
+    def get_worksite_registrations(args):
+        """fetch influenza worksites"""
+        try:
+            registrations_api_url = "{}/ods_datamart_influenza_registrations".format(current_app.config.get("ODS_URL"))
+            response_from_ods = requests.get(registrations_api_url,
+                        headers={"Authorization": current_app.config.get("ODS_AUTH_TOKEN")})
+        except Exception as e:
+            raise BusinessException(
+                {"message": "Failed to look up worksite registrations in ODS"}, HTTPStatus.INTERNAL_SERVER_ERROR
+            )
+        
+        registrations_res = response_from_ods.json()
+        if registrations_res and "value" in registrations_res and len(registrations_res["value"]) > 0:
+            return registrations_res["value"]
         raise BusinessException(
           {"message": "No worksite data found"}, HTTPStatus.NOT_FOUND
         )
