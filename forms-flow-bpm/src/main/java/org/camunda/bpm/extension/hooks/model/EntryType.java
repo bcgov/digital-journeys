981cd01d75f4b17af2aae51e847b570e1418f998
iman-fw <iman@freshworks.io>
2023-06-23 18:09:50 +0000

    crm service was added

---
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/model/EntryType.java
@@ -0,0 +1,18 @@
+package main.java.org.camunda.bpm.extension.hooks.model;
+
+public class EntryType {
+    private int id;
+
+    public EntryType(int id) {
+      this.id = id;
+    }
+
+    public int getId() {
+      return id;
+    }
+
+    public void setId(int id) {
+      this.id = id;
+    }
+    
+}
---
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/model/PrimaryContact.java
@@ -0,0 +1,17 @@
+package main.java.org.camunda.bpm.extension.hooks.model;
+
+public class PrimaryContact{
+    private int id;
+
+    public PrimaryContact(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
