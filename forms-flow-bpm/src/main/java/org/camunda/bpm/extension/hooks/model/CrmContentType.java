5af1be591cea8d600e461a11c0569e3bf1f493a9
Bhumin <bhumin.patel1@ca.ey.com>
2023-08-15 21:53:38 -0700

    DGJ-1190 Mat-Pat CRM ticket thread integration

---
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/model/CrmContentType.java
@@ -0,0 +1,18 @@
+package main.java.org.camunda.bpm.extension.hooks.model;
+
+public class CrmContentType {
+    private int id;
+
+    public CrmContentType(int id) {
+      this.id = id;
+    }
+
+    public int getId() {
+      return id;
+    }
+
+    public void setId(int id) {
+      this.id = id;
+    }
+    
+}
--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/model/CrmThread.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/hooks/model/CrmThread.java
@@ -3,10 +3,14 @@ package main.java.org.camunda.bpm.extension.hooks.model;
 public class CrmThread {
   private String text;
   private CrmEntryType crmEntryType;
+  private CrmChannel crmChannel;
+  private CrmContentType crmContentType;
 
-  public CrmThread(String text, CrmEntryType crmEntryType) {
+  public CrmThread(String text, CrmEntryType crmEntryType, CrmChannel crmChannel, CrmContentType crmContentType) {
     this.text = text;
     this.crmEntryType = crmEntryType;
+    this.crmChannel = crmChannel;
+    this.crmContentType = crmContentType;
   }
   public String getText() {
     return text;
@@ -19,5 +23,19 @@ public class CrmThread {
   }
   public void setEntryType(CrmEntryType crmEntryType) {
     this.crmEntryType = crmEntryType;
+  }
+
+  public CrmContentType getContentType() {
+    return crmContentType;
+  }
+  public void setContentType(CrmContentType crmContentType) {
+    this.crmContentType = crmContentType;
+  }
+
+  public CrmChannel getChannel() {
+    return crmChannel;
+  }
+  public void setChannel(CrmChannel crmChannel) {
+    this.crmChannel = crmChannel;
   }
 }
