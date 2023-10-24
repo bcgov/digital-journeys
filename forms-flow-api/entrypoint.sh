5d3cdd3920cc7eb3a0182889a7ff6590cc1e1f60
iman-fw <iman@freshworks.io>
2023-10-24 21:53:27 +0000

    gunicorn server timeout limit for api and pdf-generation-server were increased to 600

--- forms-flow-api/entrypoint.sh
+++ forms-flow-api/entrypoint.sh
@@ -1,4 +1,4 @@
 echo 'starting application'
 export FLASK_APP=manage.py
 flask db upgrade
-gunicorn -b :5000 'formsflow_api:create_app()' --timeout 120 --worker-class=gthread --workers=5 --threads=10
+gunicorn -b :5000 'formsflow_api:create_app()' --timeout 600 --worker-class=gthread --workers=5 --threads=10
