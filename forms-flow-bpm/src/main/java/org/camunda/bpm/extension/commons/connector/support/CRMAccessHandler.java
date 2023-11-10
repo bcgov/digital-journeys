a38233d6f7708354b410afff878afcd014d95e3b
iman-jamali-fw <77303486+iman-jamali-fw@users.noreply.github.com>
2023-11-10 09:45:43 -0800

    crm http client was replaced with java standard for debugging (#1441)

--- forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/CRMAccessHandler.java
+++ forms-flow-bpm/src/main/java/org/camunda/bpm/extension/commons/connector/support/CRMAccessHandler.java
@@ -13,6 +13,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import org.springframework.web.client.HttpClientErrorException;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
 import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId;
 
 
@@ -30,7 +36,7 @@ public class CRMAccessHandler extends AbstractAccessHandler {
     @Value("${formsflow.ai.crm.security.token}")
     private String crmAuthHeader;
 
-    public ResponseEntity<String> exchange(String url, HttpMethod method, String payload) {
+    /* public ResponseEntity<String> exchange(String url, HttpMethod method, String payload) {
         System.out.println("CRM http request handler: " + " url: " +  url + " payload: " +  payload);
         payload = (payload == null) ? new JsonObject().toString() : payload;
         System.out.println("CRM http request handler payload: " + payload);
@@ -50,6 +56,44 @@ public class CRMAccessHandler extends AbstractAccessHandler {
 
         ResponseEntity<String> response = entityMono.block();
         return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+    } */
+    public ResponseEntity<String> exchange(String url, HttpMethod method, String payload) {
+        System.out.println("CRM http request handler: " + " url: " +  url + " payload: " +  payload);
+        payload = (payload == null) ? new JsonObject().toString() : payload;
+        System.out.println("CRM http request handler payload: " + payload);
+        System.out.println("CRM http request handler crmAuthHeader: " +  crmAuthHeader);
+
+        try {
+            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Authorization", crmAuthHeader)
+                    .header("Accept", MediaType.APPLICATION_JSON_VALUE)
+                    .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                    .header("osvc-crest-application-context", "Retrieve Data");
+
+            if (method == HttpMethod.GET) {
+                requestBuilder.GET();
+            } else if (method == HttpMethod.POST) {
+                requestBuilder.POST(HttpRequest.BodyPublishers.ofString(payload));
+            } // Add other HTTP methods as needed
+
+            HttpRequest request = requestBuilder.build();
+
+            HttpResponse<String> httpResponse = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (httpResponse.statusCode() >= 400 && httpResponse.statusCode() < 500) {
+                throw new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+            } else if (httpResponse.statusCode() >= 500) {
+                throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+            return new ResponseEntity<>(httpResponse.body(), HttpStatus.valueOf(httpResponse.statusCode()));
+
+        } catch (Exception e) {
+            // Handle exceptions
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<String> exchange(String url, HttpMethod method, String payload, Boolean isUpdate) {
