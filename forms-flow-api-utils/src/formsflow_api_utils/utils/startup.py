6a1dae0870487ce7e9428ef216fed996d6101492
bhumin-fw <99269796+bhumin-fw@users.noreply.github.com>
2024-03-05 20:28:12 -0800

    Update startup.py
    Added Print statement

--- forms-flow-api-utils/src/formsflow_api_utils/utils/startup.py
+++ forms-flow-api-utils/src/formsflow_api_utils/utils/startup.py
@@ -15,6 +15,10 @@ def setup_jwt_manager(app, jwt_manager):
     """Use flask app to configure the JWTManager to work for a particular Realm."""
 
     def get_roles(a_dict):
+        print("Remove after testing")
+        print(app.config["JWT_OIDC_AUDIENCE"])
+        for k, v in a_dict.items():
+            print(k, v)
         resource = a_dict["resource_access"].get(app.config["JWT_OIDC_AUDIENCE"])
         return resource["roles"] if resource else a_dict["roles"]
 
