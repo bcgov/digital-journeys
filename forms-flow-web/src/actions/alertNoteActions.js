03e6ab15496c8b9d8431f32343ddade62ab9b1c4
Bhumin <bhumin.patel1@ca.ey.com>
2024-04-15 18:55:36 -0700

    Remove unused block

--- forms-flow-web/src/actions/alertNoteActions.js
+++ forms-flow-web/src/actions/alertNoteActions.js
@@ -1,10 +1,6 @@
 import ACTION_CONSTANTS from "./actionConstants";
 
 export const setAlertNoteData = (data) => dispatch => {
-    let cc = {
-        type:ACTION_CONSTANTS.ALERT_NOTE_DATA,
-        payload:data
-      };
   dispatch({
     type:ACTION_CONSTANTS.ALERT_NOTE_DATA,
     payload:data
