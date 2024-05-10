44a0c2dd8e2a9d2d9fd16de0713fb5a923f5293a
Jerry-EY <146475472+jerry-ey@users.noreply.github.com>
2024-05-10 14:31:08 -0700

    DGJ-1352 | snowplow hotfix | fix for link tracking (#1773)
    * fix for link tracking
    
    * fix for external link tracking
    
    * use setTimeout to fix init tracking fail

--- forms-flow-web/src/customHooks/useSnowPlow.js
+++ forms-flow-web/src/customHooks/useSnowPlow.js
@@ -6,6 +6,7 @@ import {
 import {
   refreshLinkClickTracking,
   enableLinkClickTracking,
+  LinkClickTrackingPlugin,
 } from "@snowplow/browser-plugin-link-click-tracking";
 import React from "react";
 import { useLocation } from "react-router-dom";
@@ -22,35 +23,68 @@ const initializeTracker = (endpoint) => {
     post: true,
     forceSecureTracker: true,
     contexts: { webPage: true, performanceTiming: true },
+    plugins: [LinkClickTrackingPlugin()],
   });
 
-  // plugins
   enableActivityTracking({
     minimumVisitLength: 30,
     heartbeatDelay: 30,
   });
-  enableLinkClickTracking({ pseudoClicks: true });
-  refreshLinkClickTracking();
 };
 
 const isTrackerInitialized = () => tracker !== undefined;
 
-const useLocationChange = () => {
-  const location = useLocation();
-  React.useEffect(() => {
+const startTracking = () => {
+  setTimeout(() => {
+    enableLinkClickTracking({ pseudoClicks: true });
+    refreshLinkClickTracking();
     trackPageView();
-  }, [location]);
+  }, 500);
 };
 
 const useSnowPlow = () => {
+  const location = useLocation();
+  const [isInit, setIsInit] = React.useState(isTrackerInitialized());
+  if (!isInit) {
+    initializeTracker(COLLECTOR);
+    setIsInit(true);
+  }
+  // this is for internal link click
   React.useEffect(() => {
-    const isInit = isTrackerInitialized();
-    if (!isInit) {
-      initializeTracker(COLLECTOR);
+    if (isInit) {
+      startTracking();
     }
-  }, []);
+  }, [location, isInit]);
 
-  useLocationChange();
+  // this is for external link click
+  React.useEffect(() => {
+    const handleClick = (event) => {
+      // Get the element that was clicked
+      const target = event.target;
+
+      // Check if the clicked element is a link
+      if (target.tagName === "A" && target.href) {
+        // Create a URL object from the href property
+        const href = new URL(target.href);
+        const location = window.location;
+
+        // Check if the hostname of the URL is different from the current location's hostname
+        if (href.hostname !== location.hostname) {
+          if (isInit) {
+            startTracking();
+          }
+        }
+      }
+    };
+
+    // Attach the event listener to document
+    document.addEventListener("click", handleClick);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [isInit]);
 };
 
 export default useSnowPlow;
