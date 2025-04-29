6c0e606436fdebf892da2fa41306497837c67d7b
avargas <andrew.vargas@gov.bc.ca>
2025-04-29 09:33:22 -0700

    fix line length

--- forms-flow-web/src/constants/formConstants.js
+++ forms-flow-web/src/constants/formConstants.js
@@ -2,10 +2,13 @@ export const FORM_NAMES = {
   SL_REVIEW: 'SL review',
   SENIOR_LEADER_REVIEW: 'Senior Leadership Review Form',
   TELEWORK: 'Telework agreement',
-  COMPLIANT_1_10: 'Bullying / Misuse of Authority Complaint Form (Article 1.10)',
-  INFLUENZA_WORKSITE_REGISTRATION: `${new Date().getFullYear()} Influenza Worksite Clinic Registration`,
+  COMPLIANT_1_10:
+    'Bullying / Misuse of Authority Complaint Form (Article 1.10)',
+  INFLUENZA_WORKSITE_REGISTRATION: `${new Date().getFullYear()
+    } Influenza Worksite Clinic Registration`,
   MATERNITY_AND_PARENTAL_LEAVE_FORM: 'Maternity and parental leave form',
-  MATERNITY_PARENTAL_LEAVE_AND_ALLOWANCE_FORM: 'Maternity, Parental Leave and Allowance form',
+  MATERNITY_PARENTAL_LEAVE_AND_ALLOWANCE_FORM:
+    'Maternity, Parental Leave and Allowance form',
   COI: 'Conflict of Interest Disclosure',
   ERIP: 'Early Retirement Incentive Plan',
   LEADERSHIP_SURVEY: 'Senior Leadership Feedback Survey',
@@ -15,4 +18,5 @@ export const FORM_NAMES = {
  * https://github.com/bcgov/digital-journeys/blob/main/docs/forms.md
  */
 
-export const FORM_SUPPORTED_IDENTITY_PROVIDERS_FIELD_NAME = 'formSupportedIdentityProviders';
+export const FORM_SUPPORTED_IDENTITY_PROVIDERS_FIELD_NAME =
+  'formSupportedIdentityProviders';
