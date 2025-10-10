a08a1d7370837091858cdec4f9034c2cc5e3c592
paulbothma-govbcca <paul.bothma@gov.bc.ca>
2025-10-10 08:52:44 -0700

    DGJ-2029 Changed default limit to 10

--- forms-flow-web/src/components/Form/List.js
+++ forms-flow-web/src/components/Form/List.js
@@ -114,7 +114,7 @@ const List = React.memo((props) => {
 
     return {
       forms,
-      totalForms: state.bpmForms.totalForms,
+      totalForms: state.bpmForms.totalCount - (state.bpmForms.totalCount - forms.length),
       page: state.bpmForms.page,
       limit: state.bpmForms.limit,
       sortBy: state.bpmForms.sortBy,
@@ -253,7 +253,7 @@ const List = React.memo((props) => {
 
   const handleTypeChange = (type) => {
     dispatch(setBPMFormListPage(1));
-    dispatch(setBPMFormLimit(5));
+    dispatch(setBPMFormLimit(10));
     dispatch(setBpmFormType(type));
   };
   const onClear = () => {
