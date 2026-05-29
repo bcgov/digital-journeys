d90177173ea1af663aad5fb02e94eea6218dc211
paulbothma-govbcca <paul.bothma@gov.bc.ca>
2026-05-29 16:16:24 -0700

    2027 Testing ERIP hidden form

--- forms-flow-web/src/constants/formConstants.js
+++ forms-flow-web/src/constants/formConstants.js
@@ -15,7 +15,7 @@ export const FORM_NAMES = {
   SLFS_RESPONDENT_SELECTION: 'SLFS Respondent Selection',
   VRIP: 'Voluntary Retirement Incentive Program',
   ERIPI: 'Early Retirement Incentive Plan for LTD (by invitation only)',
-  
+
 };
 /** 
  * please review "Form display name in draft and submission list" on below link
@@ -28,6 +28,6 @@ export const FORM_SUPPORTED_IDENTITY_PROVIDERS_FIELD_NAME =
 // DGJ-2029 Array with list of forms to hide from Clients  
 export const FORM_HIDDEN_LIST = (
 
-  (window._env_ && window._env_.REACT_APP_FORM_HIDDEN_LIST) || process.env.REACT_APP_FORM_HIDDEN_LIST || "SLFS_RESPONDENT_SELECTION,LEADERSHIP_SURVEY,ERIPI,VRIP"
+  "SLFS_RESPONDENT_SELECTION,LEADERSHIP_SURVEY,ERIPI,VRIP"
 
 ).split(",").map((form) => FORM_NAMES[form]).filter((form) => form && form != null);
